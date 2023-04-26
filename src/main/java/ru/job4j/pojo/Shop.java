e160a56bbc3435f1282f99b5592fe65271052b26
Nurrover <nuriksafarov00@gmail.com>
2023-04-27 01:19:43 +0400

    3.1. Массив с пустыми ячейками [#176470] 2.0

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -12,7 +12,6 @@ public class Shop {
         return index;
     }
 
-
     public static void main(String[] args) {
         Product[] products = new Product[5];
         products[0] = new Product("Milk", 10);

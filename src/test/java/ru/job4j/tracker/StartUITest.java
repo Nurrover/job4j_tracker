1698590fa4d910fcb86fd26bbadaef59ee54299d
Nurrover <nuriksafarov00@gmail.com>
2023-06-02 23:13:48 +0400

    9. Тестирование. Подготовка данных. [#182960  [#182960]]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -14,4 +14,25 @@ class StartUITest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName()).isEqualTo(expected.getName());
     }
+
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answer = {String.valueOf(item.getId()), "edited item"};
+        StartUI.editItem(new StubInput(answer), tracker);
+        Item edit = tracker.findById(item.getId());
+        assertThat(edit.getName()).isEqualTo("edited item");
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("item");
+        tracker.add(item);
+        String[] answer = {String.valueOf(item.getId())};
+        StartUI.deleteItem(new StubInput(answer), tracker);
+        assertThat(tracker.findById(item.getId())).isNull();
+    }
 }

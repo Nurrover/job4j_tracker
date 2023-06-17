b0ee8800f94f413d5f67f6624f17b928d7f7b633
Nurrover <nuriksafarov00@gmail.com>
2023-06-17 15:38:12 +0400

    0.4. Пользовательские исключения. [#219367  [#219367]]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,28 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (key.equals(value[index])) {
+                rsl = index;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("The element is not in the array");
+        }
+
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] value = {"n", "u", "k", "s"};
+        String key = "j";
+        try {
+            FindEl.indexOf(value, key);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}

59eb819c1cd6ea2ea140c6981463c0a97e981193
Nurrover <nuriksafarov00@gmail.com>
2023-08-11 13:24:54 +0400

    4. Телефонный справочник на базе ArrayList [#41598 #432238]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -16,4 +16,11 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
+
+    @Test
+    public void whenEmptyList() {
+        PhoneDictionary phones = new PhoneDictionary();
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons).isEmpty();
+    }
 }

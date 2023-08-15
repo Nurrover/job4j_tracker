0bb4d2e0c547d51589611502ffdbc1b6c5095ffa
Nurrover <nuriksafarov00@gmail.com>
2023-08-16 01:40:47 +0400

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("nurlan.safarov2018@yandex.ru", "Сафров Нурлан Афлатун оглы");
+        hashMap.put("parsentev@yandex.ru", "Petr Arsentev");
+        for (Map.Entry<String, String> entry : hashMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}

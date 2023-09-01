5bf9fcb12f61e174f237e27a290521e76f5d881e
Nurrover <nuriksafarov00@gmail.com>
2023-09-01 18:08:09 +0400

    4. Сортировка номера [#221284]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,12 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        String[] strLeft = left.split("\\.");
+        String[] strRight = right.split("\\.");
+        return Integer.compare(Integer.parseInt(strLeft[0]), Integer.parseInt(strRight[0]));
+    }
+}

eeac3cd1c068eedf4cedb933fa090df9761c66e6
Nurrover <nuriksafarov00@gmail.com>
2023-09-03 17:00:54 +0400

    2.1. Лямбда блок [#249208 #442056]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -8,8 +8,8 @@ public class LambdaUsage {
     public static void main(String[] args) {
         List<String> strings = Arrays.asList("eeeee", "a", "ccc", "dddd", "bb");
         Comparator<String> comparator = (left, right) -> {
-            int rls = Integer.compare(right.length(), left.length());
-            return rls;
+            System.out.println(left + " : " + right + " - " + left.length() + " : " + right.length());
+            return Integer.compare(right.length(), left.length());
         };
         strings.sort(comparator);
         for (String str : strings) {

75c96a6365d278e12e400a92a09f003c58ce67ad
Nurrover <nuriksafarov00@gmail.com>
2023-09-08 18:08:14 +0400

    4. Ссылки на конструкторы [#504922]

---
+++ src/main/java/ru/job4j/lambda/Model.java
@@ -0,0 +1,15 @@
+package ru.job4j.lambda;
+
+public class Model {
+    private String name;
+
+    Model() {}
+
+    Model(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

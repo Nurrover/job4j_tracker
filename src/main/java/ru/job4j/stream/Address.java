daf07b237e784e92f6b26704e2a714e0254aaeed
Nurrover <nuriksafarov00@gmail.com>
2023-09-12 17:03:08 +0400

    6. Список адресов [#110062]

---
+++ src/main/java/ru/job4j/stream/Address.java
@@ -0,0 +1,35 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+public class Address {
+
+    private String city;
+    private String streets;
+    private int home;
+    private int apartment;
+
+    public Address(String city, String streets, int home, int apartment) {
+        this.city = city;
+        this.streets = streets;
+        this.home = home;
+        this.apartment = apartment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Address address = (Address) o;
+        return home == address.home && apartment == address.apartment && Objects.equals(city, address.city) && Objects.equals(streets, address.streets);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, streets, home, apartment);
+    }
+}

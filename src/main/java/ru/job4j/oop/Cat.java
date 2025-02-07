0cef8660da6673ac6d8aa2d084e66001ae365520
Nurrover <nuriksafarov00@gmail.com>
2025-02-07 23:27:46 +0400

    Поля объекта [#166083 #410654]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,13 +1,41 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public String sound() {
         return "may-may";
     }
 
+    public void showFood() {
+        System.out.println(this.food);
+    }
+
+    public void showName() {
+        System.out.println(this.name);
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        System.out.println("There is gav's food.");
+        Cat gav = new Cat();
+        gav.eat("cutlet");
+        gav.giveNick("gav");
+        gav.showName();
+        gav.showFood();
+        System.out.println("There is black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("black");
+        black.showName();
+        black.showFood();
     }
 }

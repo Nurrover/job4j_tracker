723949296a9ee65220cbe2dafd84fbc3dde5b2fb
Nurrover <nuriksafarov00@gmail.com>
2025-02-07 21:08:28 +0400

    Правила Checkstyle [#504981 #410399]

---
+++ src/main/java/ru/job4j/tracker/checkstyle/Broken.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker.checkstyle;
+
+public class Broken {
+
+    private int size = 10;
+    private String name;
+    private String surname;
+    public static final String NEW_VALUE = "";
+
+    public Broken() { }
+
+    public void echo() { }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) { }
+
+}

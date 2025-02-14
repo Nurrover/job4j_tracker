c91ce87af9f1ecba21fc97213f5bed09a50f1f24
Nurrover <nuriksafarov00@gmail.com>
2025-02-15 00:50:07 +0400

    Конструктор [#173889 #410711]

--- src/main/java/ru/job4j/oop/Computer.java
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -5,13 +5,20 @@ public class Computer {
     private int ssd;
     private String cpu;
 
+    public Computer() { }
+
     public Computer(boolean multiMonitor, int ssd, String cpu) {
         this.multiMonitor = multiMonitor;
         this.ssd = ssd;
         this.cpu = cpu;
     }
 
-    public Computer() { }
+    public Computer(boolean multiMonitor, double ssd, String cpu) {
+        this.multiMonitor = multiMonitor;
+        this.ssd = (int) ssd;
+        this.cpu = cpu;
+    }
+
 
     public void printInfo() {
         System.out.println("Много мониторов: " + multiMonitor);
@@ -22,7 +29,7 @@ public class Computer {
     public static void main(String[] args) {
         Computer computer = new Computer(true, 500, "Intel Core I7-10700K");
         computer.printInfo();
-        Computer comp = new Computer(true, 256, "AMD Ryzen 5 3600");
+        Computer comp = new Computer(true, 1024.1, "AMD Ryzen 5 3600");
         comp.printInfo();
         Computer computer1 = new Computer();
     }

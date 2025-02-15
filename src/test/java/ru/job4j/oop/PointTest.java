ad9edc96b585df39f8e3bc219296579baa39bb33
Nurrover <nuriksafarov00@gmail.com>
2025-02-15 14:35:31 +0400

    5.0. Локальные переменные и поля. [#180148 #411139]

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -2,6 +2,7 @@ package ru.job4j.oop;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
 
 class PointTest {
 
@@ -27,6 +28,33 @@ class PointTest {
         Point b = new Point(0, 12);
         double expected = 7;
         assertThat(a.distance(b)).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenX1Y2Z3ToXMinus7YMinus2Z4Then9() {
+        Point a = new Point(1, 2, 3);
+        Point b = new Point(-7, -2, 4);
+        double rsl = a.distance3d(b);
+        double expected = 9;
+        assertThat(rsl).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void whenX0Y2Z3ToX0Y2Z4Then1() {
+        Point a = new Point(0, 2, 3);
+        Point b = new Point(0, 2, 4);
+        double rsl = a.distance3d(b);
+        double expected = 1;
+        assertThat(rsl).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void whenX2YMinus21Z7ToX6Y2Z5Then11() {
+        Point a = new Point(2, -2, 7);
+        Point b = new Point(6, 2, 5);
+        double rsl = a.distance3d(b);
+        double expected = 6;
+        assertThat(rsl).isCloseTo(expected, offset(0.001));
     }
 
 }
